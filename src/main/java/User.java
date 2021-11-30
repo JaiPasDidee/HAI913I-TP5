93ff57454ded23e42c6220f05888822ae5424a26
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2021-11-30 09:03:25 +0100

    update user

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+
 public class User {
 
     int id;
@@ -5,13 +7,15 @@ public class User {
     String email;
     String password;
     int age;
+    Command command;
 
-    public User(int id, String name, String email, String password, int age) {
+    public User(int id, String name, String email, String password, int age, Command command) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
         this.age = age;
+        this.command = command;
     }
 
     @Override
@@ -63,5 +67,35 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    //display products in a repository, where every product has an ID, a
+    //name, a price, and a expiration date
+    public void display(){
+        command.display();
+    }
+
+    //fetch a product by its ID (if no product with the provided ID exists,
+    //an exception must be thrown).
+    public void fetch(int id)throws Exception{
+        command.fetch(id);
+    }
+
+    //add a new product (if a product with the same ID already exists, an
+    //exception must be thrown)
+    public void add (Product product)throws Exception{
+        command.add(product);
+    }
+
+    //delete a product by its ID (if no product with the provided ID exists,
+    //an exception must be thrown).
+    public void delete(int id)throws Exception{
+        command.delete(id);
+    }
+
+    //update a product’s info (if no product with the provided ID exists,
+    //an exception must be thrown)
+    public void update(int id, String name, int price, Calendar expiration_date)throws Exception{
+        command.update(id,name, price, expiration_date);
     }
 }
