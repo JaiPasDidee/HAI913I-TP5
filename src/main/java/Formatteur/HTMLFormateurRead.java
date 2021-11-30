bfeef34783a8fe17e6b000255fee36845ffe6280
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2021-11-30 09:43:16 +0100

    update HTMLFormateurRead

--- src/main/java/Formatteur/HTMLFormateurRead.java
+++ src/main/java/Formatteur/HTMLFormateurRead.java
@@ -5,6 +5,7 @@ import utils.User;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class HTMLFormateurRead extends Formatter {
@@ -16,7 +17,40 @@ public class HTMLFormateurRead extends Formatter {
 
     @Override
     public String format(LogRecord record) {
-        return null;
+        StringBuffer buf = new StringBuffer(1000);
+        buf.append("\t\t\t\t<th style=\"width:15%\">"+ java.time.LocalTime.now()+"</th>\n");
+        buf.append("\t\t\t\t<th style=\"width:75%\">"+ record.getMessage()+"</th>\n");
+        buf.append("\t\t\t</tr>\n");
+        buf.append("<tr>/n");
+        // color any levels >= WARNING in red
+        if(record.getLevel().intValue() >= Level.WARNING.intValue()) {
+            buf.append("\t<td style=\"color:red\">");
+            buf.append("<b>");
+            buf.append(record.getLevel());
+            buf.append("</b>");
+        }
+        else {
+            buf.append("\t<td>");
+            buf.append(record.getLevel());
+        }
+        buf.append("</td>\n");
+        buf.append("\t<td>");
+        buf.append("User id : "+ user.getId());
+        buf.append("</td>\n");
+        buf.append("\t<td>");
+        buf.append("User name : "+ user.getName());
+        buf.append("</td>\n");
+        buf.append("\t<td>");
+        buf.append("User age : "+ user.getAge());
+        buf.append("</td>\n");
+        buf.append("\t<td>");
+        buf.append("User email : "+ user.getEmail());
+        buf.append("</td>\n");
+        buf.append("\t<td>");
+        buf.append("Profile that mostly performed read operations");
+        buf.append("</td>\n");
+        buf.append("</tr>\n");
+        return buf.toString();
     }
 
     @Override
@@ -35,15 +69,15 @@ public class HTMLFormateurRead extends Formatter {
         buf.append("\t\t<h1>" + (new Date()) + "\n");
         buf.append("\t\t<table border=\"0\" cellpadding=\"5\" cellspacing=\"3\">\n");
         buf.append("\t\t\t<tr align=\"left\">\n");
-        buf.append("\t\t\t\t<th style=\"width:10%\">LogLevel</th>\n");
-        buf.append("\t\t\t\t<th style=\"width:15%\">"+ java.time.LocalTime.now()+"</th>\n");
-        buf.append("\t\t\t\t<th style=\"width:75%\">LogMessage</th>\n");
-        buf.append("\t\t\t</tr>\n");
         return buf.toString();
     }
 
     @Override
     public String getTail(Handler h) {
-        return super.getTail(h);
+        StringBuffer buf = new StringBuffer(100);
+        buf.append("\t\t</table>\n");
+        buf.append("\t</body>\n");
+        buf.append("</html>");
+        return buf.toString();
     }
 }
