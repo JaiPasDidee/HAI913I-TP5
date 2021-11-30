e80bb67ef2ff3fac79b06898352349cc2cc4e018
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2021-11-30 09:29:50 +0100

    move package

--- src/main/java/Formatteur/HTMLFormateurRead.java
+++ src/main/java/Formatteur/HTMLFormateurRead.java
@@ -2,6 +2,7 @@ package Formatteur;
 
 import utils.User;
 
+import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
@@ -20,7 +21,25 @@ public class HTMLFormateurRead extends Formatter {
 
     @Override
     public String getHead(Handler h) {
-        return super.getHead(h);
+        StringBuffer buf = new StringBuffer(10000);
+        buf.append("<!DOCTYPE html>\n");
+        buf.append("\t<head>\n");
+        buf.append("\t\t<style>\n");
+        buf.append("\t\ttable { width: 100% }\n");
+        buf.append("\t\tth { font: bold 10pt Tahoma; }\n");
+        buf.append("\t\ttd { font: normal 10pt Tahoma; }\n");
+        buf.append("\t\th1 { font: normal 11pt Tahoma; }\n");
+        buf.append("\t\t</style>\n");
+        buf.append("\t</head>\n");
+        buf.append("\t<body>\n");
+        buf.append("\t\t<h1>" + (new Date()) + "\n");
+        buf.append("\t\t<table border=\"0\" cellpadding=\"5\" cellspacing=\"3\">\n");
+        buf.append("\t\t\t<tr align=\"left\">\n");
+        buf.append("\t\t\t\t<th style=\"width:10%\">LogLevel</th>\n");
+        buf.append("\t\t\t\t<th style=\"width:15%\">"+ java.time.LocalTime.now()+"</th>\n");
+        buf.append("\t\t\t\t<th style=\"width:75%\">LogMessage</th>\n");
+        buf.append("\t\t\t</tr>\n");
+        return buf.toString();
     }
 
     @Override
---
+++ src/main/java/utils/User.java
@@ -0,0 +1,103 @@
+package utils;
+
+import java.util.Calendar;
+
+public class User {
+
+    int id;
+    String name;
+    String email;
+    String password;
+    int age;
+    Command command;
+
+    public User(int id, String name, String email, String password, int age, Command command) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.command = command;
+    }
+
+    @Override
+    public String toString() {
+        return "utils.User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //display products in a repository, where every product has an ID, a
+    //name, a price, and a expiration date
+    public void display(){
+        command.display();
+    }
+
+    //fetch a product by its ID (if no product with the provided ID exists,
+    //an exception must be thrown).
+    public void fetch(int id)throws Exception{
+        command.fetch(id);
+    }
+
+    //add a new product (if a product with the same ID already exists, an
+    //exception must be thrown)
+    public void add (Product product)throws Exception{
+        command.add(product);
+    }
+
+    //delete a product by its ID (if no product with the provided ID exists,
+    //an exception must be thrown).
+    public void delete(int id)throws Exception{
+        command.delete(id);
+    }
+
+    //update a product’s info (if no product with the provided ID exists,
+    //an exception must be thrown)
+    public void update(int id, String name, int price, Calendar expiration_date)throws Exception{
+        command.update(id,name, price, expiration_date);
+    }
+}
